8805053434e2d8cda9076be764905f14f2b2e88f
yf <r07943148@ntu.edu.tw>
2021-04-13 07:45:37 +0800

    add 10.6 constraints & report

---
+++ 10.6.1_multi_block/solution1/script.tcl
@@ -0,0 +1,21 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project hls_laba_multi_block
+set_top dec2_2stage
+add_files multirate_filters/src/10.6.1_multi_block/dec2_2stage.h
+add_files multirate_filters/src/10.6.1_multi_block/dec2_multi_block.cpp
+add_files multirate_filters/src/10.6.1_multi_block/decimate.hpp
+add_files multirate_filters/src/10.6.1_multi_block/helper.h
+add_files multirate_filters/src/10.6.1_multi_block/shift_class.h
+add_files -tb multirate_filters/src/10.6.1_multi_block/MultiBlockTester.cpp
+open_solution "solution1"
+set_part {xc7z020clg400-1}
+create_clock -period 5 -name default
+source "./hls_laba_multi_block/solution1/directives.tcl"
+csim_design
+csynth_design
+cosim_design
+export_design -format ip_catalog
